1cf1ec2996f6997d4deee653337d0ba5fe26d940
Huseyin-can-24 <huseyincan.goktepe@std.yeditepe.edu.tr>
2025-04-07 17:54:01 +0300

    geometre 7/4/2025

---
+++ geo.py
@@ -0,0 +1,7 @@
+
+def toplamHacim(r1,h1,r2,h2):
+  pi=3.14
+  v1=(pi*(r1**2)*h1)/3
+  v2=pi*(r2**2)*h2
+  toplam = v1+v2
+  return toplam
--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-def toplama(a,b):
-  toplama=a+b
-  print(toplama)
-def cikarma(a,b):
-  cikarma=a-b
-  print(cikarma)
-def carpma(a,b):
-  carpma=a*b
-  print(carpma)
-def bolme(a,b):
-  bolme=a/b
-  print(bolme)
-while True: 
-  a=int(input("Sayı1"))
-  b=int(input("Sayı2"))
-  operasyon=input("operasyon")
-  if operasyon=="+":
-    toplama(a,b)
-  elif  operasyon=="-":
-   cikarma(a,b)
-  elif  operasyon=="*":
-   carpma(a,b)
-  elif  operasyon=="/":
-   bolme(a,b)
-  else:
-   print("Hatalı İşlem")
-  f=input("Devam mı?")
-  if f=="Y" or f=="y":
-   continue
-  elif f=="N" or f=="n":
-    break
-  else:
-    break
-print("Ende")
+import geo
+miktar=int(input("kaç tane minare?"))
+r1=int(input("koni yaricapi?"))
+h1=int(input("koni yükseklik?"))
+
+r2=int(input("silindir yaricapi?"))
+h2=int(input("silindir yükseklik?"))
+result= miktar*geo.toplamHacim(r1,h1,r2,h2)
+print(result)
