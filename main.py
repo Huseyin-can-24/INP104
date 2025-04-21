d8467548c3a2762f5f384318afae60add8bfef4b
Huseyin-can-24 <huseyincan.goktepe@std.yeditepe.edu.tr>
2025-04-21 16:27:16 +0300

    21/04 uygulama 1.Soru

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-import geo
-miktar=int(input("kaç tane minare?"))
-r1=int(input("koni yaricapi?"))
-h1=int(input("koni yükseklik?"))
-
-r2=int(input("silindir yaricapi?"))
-h2=int(input("silindir yükseklik?"))
-result= miktar*geo.toplamHacim(r1,h1,r2,h2)
-print(result)
+a=str(input("yazı yaz"))
+print(len(a))
